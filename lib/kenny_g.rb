2f9787c69aab6e701a4eb8733b9b9bd0f04b247f
Chae <chaemobile@gmail.com>
2015-07-05 13:47:04 -0400

    Setup specs

--- lib/kenny_g.rb
+++ lib/kenny_g.rb
@@ -1,5 +1,2 @@
-require "kenny_g/version"
-
-module KennyG
-  # Your code goes here...
+class KennyG
 end
---
+++ spec/kenny_g_spec.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+require 'kenny_g'
+
+describe KennyG do
+  it "works" do
+    expect(KennyG.new).to be_truthy
+  end
+end
