7a9ae69f488e23fa3ae8a16b1487b8560a314929
Chae <chaemobile@gmail.com>
2015-07-05 15:37:59 -0400

    Moves specs up an abstraction level

--- spec/kenny_g_spec.rb
+++ spec/kenny_g_spec.rb
@@ -1,13 +1,25 @@
 require 'spec_helper'
 require 'kenny_g'
 require 'kenny_g/scorekeeper'
+require 'kenny_g/player'
 
 describe KennyG do
+  let(:params)  { { winning_score: 100 } }
+  let(:kenny_g) { KennyG::please_be_the_scorekeeper(params) }
+
   describe '.please_be_the_scorekeeper' do
-    let(:params)  { { winning_score: 100 } }
-    let(:kenny_g) { KennyG::please_be_the_scorekeeper(params) }
     it 'sets the winning score with valid params' do
       expect(kenny_g.winning_score).to eq 100
+    end
+  end
+
+  describe '#add_player' do
+    let(:player_params) { { name: 'Esme' } }
+    let(:expected_players) { [Scorekeeper::Player.new(player_params)] }
+
+    it 'increments the players count' do
+      expect{ kenny_g.add_player(player_params) }
+        .to change{ kenny_g.players.size }.by 1
     end
   end
 end
