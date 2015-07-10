48b0963eca33c3702fdfe5f10c199d909bf12620
Chae <chaemobile@gmail.com>
2015-07-10 10:44:13 -0400

    Require players on intialization

--- lib/kenny_g/scorekeeper.rb
+++ lib/kenny_g/scorekeeper.rb
@@ -4,7 +4,7 @@ class Scorekeeper
   attr_reader :starting_score, :players, :winning_score
   private :starting_score
 
-  def initialize(players: [], winning_score:)
+  def initialize(players:, winning_score:)
     @winning_score = winning_score
     @players = players
   end
--- spec/kenny_g_spec.rb
+++ spec/kenny_g_spec.rb
@@ -4,12 +4,17 @@ require 'kenny_g/scorekeeper'
 require 'kenny_g/player'
 
 describe KennyG do
-  let(:params)  { { winning_score: 100 } }
+  let(:params)  { { winning_score: 100,
+                    players: [] } }
   let(:kenny_g) { KennyG::please_be_the_scorekeeper(params) }
 
   describe '.please_be_the_scorekeeper' do
-    it 'sets the winning score with valid params' do
+    it 'sets the winning score' do
       expect(kenny_g.winning_score).to eq 100
+    end
+
+    it 'delcares players' do
+      expect(kenny_g.players).to eq []
     end
   end
 
