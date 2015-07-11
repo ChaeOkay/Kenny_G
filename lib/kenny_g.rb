deb3ca242c72e1dced91c45e45bd150f8b7a1ee7
Chae <chaemobile@gmail.com>
2015-07-11 10:00:22 -0400

    KennyG require name

--- lib/kenny_g.rb
+++ lib/kenny_g.rb
@@ -2,6 +2,10 @@ require 'kenny_g/game_setup'
 
 module KennyG
   def self.please_be_the_scorekeeper(options)
-    GameSetup.new(options)
+    players = options[:players] ||= []
+    names_collection = Array[players].flatten
+    raise 'at least one player name must be provided' if names_collection.empty?
+
+    GameSetup.new(players: names_collection, winning_score: options.fetch(:winning_score))
   end
 end
--- spec/kenny_g_spec.rb
+++ spec/kenny_g_spec.rb
@@ -10,5 +10,11 @@ describe KennyG do
       expect(GameSetup).to receive(:new).with(params)
       described_class.please_be_the_scorekeeper(params)
     end
+
+    it 'raises error if no name is provided' do
+      params = { winning_score: 100, players: nil }
+      expect{ described_class.please_be_the_scorekeeper(params) }
+        .to raise_error.with_message('at least one player name must be provided')
+    end
   end
 end
