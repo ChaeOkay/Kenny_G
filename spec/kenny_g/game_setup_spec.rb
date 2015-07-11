60d88f53dd6f6fe07e2d62b9c062d26ca0e0827c
Chae <chaemobile@gmail.com>
2015-07-11 09:05:40 -0400

    Add unit test for GameSetup service object

---
+++ spec/kenny_g/game_setup_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+require 'kenny_g/game_setup'
+
+describe GameSetup do
+  let(:options)  { { winning_score: 100, players: ['beatrix'] } }
+  let(:game_setup) { described_class.new(options) }
+
+  describe '#add_player' do
+    it 'increments the player count' do
+      expect{ game_setup.add_player(name: 'lilly') }
+        .to change{ game_setup.players.size }.by 1
+    end
+  end
+end
