0d388484de9272e52870695d53b6b8187bcf1250
Chae <chaemobile@gmail.com>
2015-07-12 10:18:01 -0400

    Instantiate an instance of kenny g

--- lib/kenny_g.rb
+++ lib/kenny_g.rb
@@ -2,10 +2,22 @@ require 'kenny_g/game'
 require 'kenny_g/players'
 require 'kenny_g/sanitized_options'
 
-module KennyG
+class KennyG
+  attr_reader :game
+  private :game
+
   def self.please_be_the_scorekeeper(options)
     sanitized_options = SanitizedOptions.new(options)
     players = Players.collection(sanitized_options.players)
-    Game.new(players: players, winning_score: sanitized_options.winning_score)
+    game = Game.new(players: players, winning_score: sanitized_options.winning_score)
+    new(game: game)
+  end
+
+  def initialize(game:)
+    @game = game
+  end
+
+  def add_player(name:)
+    game.add_player(name: name)
   end
 end
--- spec/kenny_g_spec.rb
+++ spec/kenny_g_spec.rb
@@ -1,15 +1,17 @@
 require 'spec_helper'
 
 describe KennyG do
-  let(:players) { Players.collection(['shelly']) }
-  let(:winning_score) { 100 }
-  let(:expected_arguments) { { players: players, winning_score: winning_score } }
-  let(:options)  { { winning_score: winning_score, players: ['shelly'] } }
+  context 'game setup' do
+    let(:players) { Players.collection(['shelly']) }
+    let(:winning_score) { 100 }
+    let(:expected_arguments) { { players: players, winning_score: winning_score } }
+    let(:options)  { { winning_score: winning_score, players: ['shelly'] } }
 
-  describe '.please_be_the_scorekeeper' do
-    it 'creates a game' do
-      expect(Game).to receive(:new).with(expected_arguments)
-      described_class.please_be_the_scorekeeper(options)
+    describe '.please_be_the_scorekeeper' do
+      it 'creates a game' do
+        expect(Game).to receive(:new).with(expected_arguments)
+        described_class.please_be_the_scorekeeper(options)
+      end
     end
   end
 end
