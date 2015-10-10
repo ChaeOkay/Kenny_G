21875e38cb3bb05772bc36e3aa7ec9a8f71600c6
chaeokay <chae@bignerdranch.com>
2015-10-10 14:51:05 -0400

    Adds outline of pregame expectations

---
+++ spec/kenny_g/pre_game_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe PreGame do
+  #a pregame has players
+  #a pregame can add players
+  #a pregame can remove players
+  #
+  #a pregam has a winning score
+  #a pregame can redefine the winning score
+  #
+  #a valid pregame can become a game
+  #an invalid pregame has errors
+end
