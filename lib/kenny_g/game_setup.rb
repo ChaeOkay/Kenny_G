735ef428bba0b43445ef54a0ddd6980751f2ed2e
chaeokay <chae@bignerdranch.com>
2015-10-22 20:25:12 -0500

    KennyG Please be the score keeper has optional params

--- lib/kenny_g/game_setup.rb
+++ lib/kenny_g/game_setup.rb
@@ -1,7 +1,7 @@
 class GameSetup
   attr_reader :players, :winning_score
 
-  def initialize(players:, winning_score:)
+  def initialize(players: [], winning_score: 0)
     @players = players
     @winning_score = winning_score
   end
