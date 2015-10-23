2f0dad235e1413dac848b99c4e374264f1a7230b
chaeokay <chae@bignerdranch.com>
2015-10-22 19:11:29 -0500

    Identify initialize as what's being tested

--- spec/kenny_g/game_setup_spec.rb
+++ spec/kenny_g/game_setup_spec.rb
@@ -1,13 +1,14 @@
 require 'spec_helper'
 
 describe GameSetup do
+  describe '#initialize' do
+    it 'has optional params' do
+      expect(described_class.new).to be_a(GameSetup)
+    end
 
-  it 'can be instantiated without players or a winning score' do
-    expect(described_class.new).to be_a(GameSetup)
-  end
-
-  it 'can accept players and a winning score' do
-    params = { players: ['jennay', 'mad max'], winning_score: 10 }
-    expect(described_class.new(params)).to be_a(GameSetup)
+    it 'accepts players and or a winning score' do
+      params = { players: ['jennay', 'mad max'], winning_score: 10 }
+      expect(described_class.new(params)).to be_a(GameSetup)
+    end
   end
 end
