33629bf5416841006a645948c23867f88a1dc610
chaeokay <chae@bignerdranch.com>
2015-10-22 20:30:21 -0500

    Name variables to reflect object

--- spec/kenny_g_spec.rb
+++ spec/kenny_g_spec.rb
@@ -3,16 +3,16 @@ require 'spec_helper'
 describe KennyG do
   describe '.please_be_the_scorekeeper' do
     it 'returns a game setup given no params' do
-      kenny_g = described_class.please_be_the_scorekeeper
-      expect(kenny_g.players).to eq []
-      expect(kenny_g.winning_score).to eq 0
+      game_setup = described_class.please_be_the_scorekeeper
+      expect(game_setup.players).to eq []
+      expect(game_setup.winning_score).to eq 0
     end
 
     it 'returns a game setup given a winning score and players' do
       params = { players: ['shelly'], winning_score: 100 }
-      kenny_g = described_class.please_be_the_scorekeeper(params)
-      expect(kenny_g.players).to eq ['shelly']
-      expect(kenny_g.winning_score).to eq 100
+      game_setup = described_class.please_be_the_scorekeeper(params)
+      expect(game_setup.players).to eq ['shelly']
+      expect(game_setup.winning_score).to eq 100
     end
   end
 end
