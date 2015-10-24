240f88d763af58075c5371cf2016accbe5f81f0b
chaeokay <chae@bignerdranch.com>
2015-10-24 16:53:00 -0500

    Adds spec around game play details

--- lib/kenny_g/game_play.rb
+++ lib/kenny_g/game_play.rb
@@ -1,7 +1,25 @@
 require_relative 'game'
+require_relative 'game_setup'
 
 class GamePlay < Game
-  def initialize(game_setup)
+  def initialize(game_setup: GameSetup.new)
     @game_setup = game_setup
+    @game_players = [game_setup.details[:players]].flatten
+    @game_winning_score = game_setup.details[:winning_score]
+  end
+
+  private
+  attr_reader :game_players, :game_winning_score
+
+  def players
+    game_players
+  end
+
+  def status
+    :game_play
+  end
+
+  def winning_score
+    game_winning_score
   end
 end
--- spec/kenny_g/game_play_spec.rb
+++ spec/kenny_g/game_play_spec.rb
@@ -1,4 +1,12 @@
 require 'spec_helper'
 
 describe GamePlay do
+  subject(:game_play) { described_class.new }
+
+  describe '#details' do
+    it 'displays game play players, winning_score, and status' do
+      details = { players: [], winning_score: 0, status: :game_play }
+      expect(game_play.details).to eq details
+    end
+  end
 end
