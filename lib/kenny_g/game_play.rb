ce4f00b6d9b6c452df636925823dbeeb1d9d37dd
chaeokay <chae@bignerdranch.com>
2015-10-24 18:51:49 -0500

    GamePlay requires a setup object in order to be instantiated

--- lib/kenny_g/game_play.rb
+++ lib/kenny_g/game_play.rb
@@ -2,8 +2,7 @@ require_relative 'game'
 require_relative 'game_setup'
 
 class GamePlay < Game
-  def initialize(game_setup: GameSetup.new)
-    @game_setup = game_setup
+  def initialize(game_setup:)
     @game_players = [game_setup.details[:players]].flatten
     @game_winning_score = game_setup.details[:winning_score]
   end
--- spec/kenny_g/game_play_spec.rb
+++ spec/kenny_g/game_play_spec.rb
@@ -1,12 +1,14 @@
 require 'spec_helper'
 
 describe GamePlay do
-  subject(:game_play) { described_class.new }
+  let(:details) { { players: ['hi'], winning_score: 200 } }
+  let(:game_setup) { double('GameSetup', details: details) }
+  subject(:game_play) { described_class.new(game_setup: game_setup) }
 
   describe '#details' do
     it 'displays game play players, winning_score, and status' do
-      details = { players: [], winning_score: 0, status: :game_play }
-      expect(game_play.details).to eq details
+      expected_details = details.merge(status: :game_play)
+      expect(game_play.details).to eq expected_details
     end
   end
 end
