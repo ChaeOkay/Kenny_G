386e41a0eba84de4161b670cb6ba2e4b84720e12
chaeokay <chae@bignerdranch.com>
2015-10-25 10:37:55 -0500

    Add alias method to start game
    * after testing in irb, the first thing I wrote was start game. So...

--- lib/kenny_g.rb
+++ lib/kenny_g.rb
@@ -10,6 +10,7 @@ module KennyG
     def play_game(game_params = {})
       QuickGameSetup.new(game_params).play_game
     end
+    alias_method :start_game, :play_game
 
     def details
       "Nothing to do yet! Kick things off by running `KennyG.game_setup` or `KennyG.play_game`."
